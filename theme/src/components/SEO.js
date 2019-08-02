a3680968dbdeb1a9645ba478e00b51383d004b06
Nicky Meuleman <nicky.meuleman@gmail.com>
2019-08-02 17:20:38 +0200

    refactor: staticquery to usestaticquery

--- theme/src/components/SEO.js
+++ theme/src/components/SEO.js
@@ -1,22 +1,7 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import * as path from "path"
-
-const query = graphql`
-  query GetSiteMetadata {
-    site {
-      siteMetadata {
-        siteUrl
-        title
-        description
-        social {
-          twitter
-        }
-      }
-    }
-  }
-`
 
 const SEO = ({
   meta,
@@ -27,97 +12,108 @@ const SEO = ({
   lang,
   keywords,
   basePath,
-}) => (
-  <StaticQuery
-    query={query}
-    render={data => {
-      const { siteMetadata } = data.site
-      const metaDescription = description || siteMetadata.description
-      const metaImage = image
-        ? path.join(`${siteMetadata.siteUrl}`, `${image}`)
-        : null
-      let url = path.join(`${siteMetadata.siteUrl}`, basePath || "", slug || "")
-      if (url.endsWith("/")) {
-        // if url ends in "/", remove it
-        url = url.slice(0, -1)
+}) => {
+  const result = useStaticQuery(graphql`
+    query GetSiteMetadata {
+      site {
+        siteMetadata {
+          siteUrl
+          title
+          description
+          social {
+            twitter
+          }
+        }
       }
-      return (
-        <Helmet
-          htmlAttributes={{ lang: lang || "en" }}
-          {...(title
+    }
+  `)
+
+  const { siteMetadata } = result.site
+  const metaDescription = description || siteMetadata.description
+  const metaImage = image
+    ? path.join(`${siteMetadata.siteUrl}`, `${image}`)
+    : null
+  let url = path.join(`${siteMetadata.siteUrl}`, basePath || "", slug || "")
+  if (url.endsWith("/")) {
+    // if url ends in "/", remove it
+    url = url.slice(0, -1)
+  }
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang: lang || "en" }}
+      {...(title
+        ? {
+            titleTemplate: `%s - ${siteMetadata.title}`,
+            title,
+          }
+        : {
+            title: siteMetadata.title,
+          })}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription,
+        },
+        {
+          property: "og:url",
+          content: url,
+        },
+        {
+          property: "og:title",
+          content: title || siteMetadata.title,
+        },
+        {
+          name: "og:description",
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:creator",
+          content: siteMetadata.social.twitter,
+        },
+        {
+          name: "twitter:title",
+          content: title || siteMetadata.title,
+        },
+        {
+          name: "twitter:description",
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          metaImage
+            ? [
+                {
+                  property: "og:image",
+                  content: metaImage,
+                },
+                {
+                  name: "twitter:image",
+                  content: metaImage,
+                },
+              ]
+            : []
+        )
+        .concat(
+          keywords.length > 0
             ? {
-                titleTemplate: `%s - ${siteMetadata.title}`,
-                title,
+                name: `keywords`,
+                content: keywords.join(`, `),
               }
-            : {
-                title: siteMetadata.title,
-              })}
-          meta={[
-            {
-              name: "description",
-              content: metaDescription,
-            },
-            {
-              property: "og:url",
-              content: url,
-            },
-            {
-              property: "og:title",
-              content: title || siteMetadata.title,
-            },
-            {
-              name: "og:description",
-              content: metaDescription,
-            },
-            {
-              property: `og:type`,
-              content: `website`,
-            },
-            {
-              name: "twitter:card",
-              content: "summary",
-            },
-            {
-              name: "twitter:creator",
-              content: siteMetadata.social.twitter,
-            },
-            {
-              name: "twitter:title",
-              content: title || siteMetadata.title,
-            },
-            {
-              name: "twitter:description",
-              content: metaDescription,
-            },
-          ]
-            .concat(
-              metaImage
-                ? [
-                    {
-                      property: "og:image",
-                      content: metaImage,
-                    },
-                    {
-                      name: "twitter:image",
-                      content: metaImage,
-                    },
-                  ]
-                : []
-            )
-            .concat(
-              keywords.length > 0
-                ? {
-                    name: `keywords`,
-                    content: keywords.join(`, `),
-                  }
-                : []
-            )
-            .concat(meta)}
-        />
-      )
-    }}
-  />
-)
+            : []
+        )
+        .concat(meta)}
+    />
+  )
+}
 
 SEO.defaultProps = {
   meta: [],
