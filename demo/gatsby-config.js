914226999cde99866da830050301db98625d65f9
Nicky Meuleman <nicky.meuleman@gmail.com>
2019-09-18 00:06:28 +0200

    chore: lint demo

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://gatsby-theme-nicky-blog.netlify.com",
-    title: "Gatsby Theme Blog by NMeuleman",
-    description: "A demo site for gatsby-theme-nicky-blog",
+    siteUrl: `https://gatsby-theme-nicky-blog.netlify.com`,
+    title: `Gatsby Theme Blog by NMeuleman`,
+    description: `A demo site for gatsby-theme-nicky-blog`,
     social: {
-      twitter: "@NMeuleman",
+      twitter: `@NMeuleman`,
     },
   },
   plugins: [
     {
-      resolve: "@nickymeuleman/gatsby-theme-blog",
+      resolve: `@nickymeuleman/gatsby-theme-blog`,
       options: {
-        contentPath: "posts",
-        assetPath: "data",
-        // basePath: "blog",
+        contentPath: `posts`,
+        assetPath: `data`,
+        // basePath: `blog`,
         // not specifying the pagination object will create a single listing page
         pagination: {
           // postsPerPage: 10,
-          // prefixPath: "page"
-        }
+          // prefixPath: `page`
+        },
       },
     },
   ],
