7833de97d5bb39715e3caaa20b63c702352e3bb6
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-01-27 21:59:41 +0100

    refactor: simplify

--- theme/src/components/layout.tsx
+++ theme/src/components/layout.tsx
@@ -4,18 +4,10 @@ import { MDXProvider } from "@mdx-js/react"
 import Header from "./Header"
 import Main from "./Main"
 import GlobalStyles from "./GlobalStyles"
-import * as MdxComponents from "./mdx-components"
-
-const mdxComponents = Object.entries(MdxComponents).reduce(
-  (acc, [name, Component]) => ({
-    ...acc,
-    [name]: (props: any) => <Component {...props} />,
-  }),
-  {}
-) as any
+import * as mdxComponents from "./mdx-components"
 
 const Layout: React.FC = ({ children, ...props }) => (
-  <MDXProvider components={mdxComponents}>
+  <MDXProvider components={mdxComponents as any}>
     <ThemeLayout>
       <GlobalStyles />
       <Header {...props} />
