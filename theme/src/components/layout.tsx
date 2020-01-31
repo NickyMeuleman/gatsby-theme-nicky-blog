677d972aaf3b1959440660c04ac615392f76418e
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-01-31 17:41:37 +0100

    refactor: remove emotion/core and mdx-js/react dependencies

--- theme/src/components/layout.tsx
+++ theme/src/components/layout.tsx
@@ -1,27 +1,21 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx, Box } from "theme-ui"
-import { MDXProvider } from "@mdx-js/react"
 import Header from "./Header"
 import Main from "./Main"
-import GlobalStyles from "./GlobalStyles"
-import * as mdxComponents from "./mdx-components"
 
 const Layout: React.FC = ({ children, ...props }) => (
-  <MDXProvider components={mdxComponents as any}>
-    <Box
-      sx={{
-        minHeight: `100vh`,
-        display: `flex`,
-        flexDirection: `column`,
-        variant: `styles.Layout`,
-      }}
-    >
-      <GlobalStyles />
-      <Header {...props} />
-      <Main {...props}>{children}</Main>
-    </Box>
-  </MDXProvider>
+  <Box
+    sx={{
+      minHeight: `100vh`,
+      display: `flex`,
+      flexDirection: `column`,
+      variant: `styles.Layout`,
+    }}
+  >
+    <Header {...props} />
+    <Main {...props}>{children}</Main>
+  </Box>
 )
 
 export default Layout
--- theme/src/gatsby-plugin-theme-ui/components.js
+++ theme/src/gatsby-plugin-theme-ui/components.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Prism from "@theme-ui/prism"
 import Headings from "../components/Headings"
+import * as MdxComponents from "../components/mdx-components"
 
 export default {
   pre: ({ children }) => <>{children}</>,
   code: Prism,
   ...Headings,
+  ...MdxComponents,
 }
