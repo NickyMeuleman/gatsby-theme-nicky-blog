3d3ecd8d381c687bb26b757a96278787565522ac
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-02-01 17:11:11 +0100

    fix: manually add mdxprovider in layout

--- theme/src/components/layout.tsx
+++ theme/src/components/layout.tsx
@@ -1,21 +1,25 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx, Box } from "theme-ui"
+import { MdxEmbedProvider } from "@pauliescanlon/gatsby-mdx-embed"
 import Header from "./Header"
 import Main from "./Main"
 
+// manually add MdxEmbedProvider from gatsby-mdx-embed. The gatsby-browser it uses seems to not get applied correctly.
 const Layout: React.FC = ({ children, ...props }) => (
-  <Box
-    sx={{
-      minHeight: `100vh`,
-      display: `flex`,
-      flexDirection: `column`,
-      variant: `styles.Layout`,
-    }}
-  >
-    <Header {...props} />
-    <Main {...props}>{children}</Main>
-  </Box>
+  <MdxEmbedProvider>
+    <Box
+      sx={{
+        minHeight: `100vh`,
+        display: `flex`,
+        flexDirection: `column`,
+        variant: `styles.Layout`,
+      }}
+    >
+      <Header {...props} />
+      <Main {...props}>{children}</Main>
+    </Box>
+  </MdxEmbedProvider>
 )
 
 export default Layout
