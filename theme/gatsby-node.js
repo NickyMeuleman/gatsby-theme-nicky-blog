dbd660bdaf02fa437d0ed974eeec9a1c23c71145
Nicky Meuleman <nicky.dev@outlook.com>
2021-03-05 20:58:07 +0100

    feat: upgrade to gatsby v3

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -599,3 +599,16 @@ exports.createPages = async ({ actions, graphql, reporter }, options) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+       alias: {
+          path: require.resolve("path-browserify")
+       },
+      fallback: {
+        fs: false,
+      }
+   }
+  })
+}
