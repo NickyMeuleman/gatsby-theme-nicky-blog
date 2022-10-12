d62bca713a05ac463656e69c327ec48086619bf1
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-12 17:18:55 +0200

    chore(:skip): do not lint proptypes in ts :skip :skip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -49,6 +49,7 @@ const eslintConfig = {
                 project: ["./tsconfig.json"],
             },
             rules: {
+                "react/prop-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/ban-ts-comment": "off",
