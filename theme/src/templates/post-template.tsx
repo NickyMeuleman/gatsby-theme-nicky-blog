1c28380492869b3f4d583d65b795595b585e000e
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-13 18:49:05 +0200

    chore: query data in post template

--- theme/src/templates/post-template.tsx
+++ theme/src/templates/post-template.tsx
@@ -1,36 +1,52 @@
-// import React from "react";
 import { graphql } from "gatsby";
-// import { MDXProvider } from "@mdx-js/react";
-// import { Link } from "gatsby";
 
 import PostComponent from "../components/test-post";
 
 export default PostComponent;
 
 // gatsby-plugin-mdx_10003 Invalid AST. happens when you try any TS things like types
-// const shortcodes = { Link }; // Provide common components here
 
-// const PageTemplate: React.FC<{
-//   data: any;
-//   children: any;
-// }> = ({ data, children }) => {
-//   console.log(children);
-//   return (
-//     <>
-//       <h1>{data.mdx.frontmatter.title}</h1>
-//       <MDXProvider components={shortcodes}>{children}</MDXProvider>
-//     </>
-//   );
-// };
-
-export const query = graphql`
-  query ($id: String!) {
-    mdx(id: { eq: $id }) {
-      frontmatter {
-        title
+export const testPostTemplateQuery = graphql`
+  query testPostTemplateQuery($slug: String!) {
+    blogPost(slug: { eq: $slug }) {
+      excerpt
+      id
+      slug
+      date
+      updatedAt
+      canonicalUrl
+      keywords
+      tableOfContents
+      series {
+        name
+        posts {
+          id
+          date
+          slug
+          title
+          instance {
+            basePath
+          }
+        }
+      }
+      tags {
+        name
+        slug
+      }
+      authors {
+        shortName
+        name
+        twitter
+      }
+      instance {
+        basePath
+      }
+      title
+      cover {
+        childImageSharp {
+          gatsbyImageData(layout: FULL_WIDTH)
+        }
       }
     }
   }
 `;
-
-// export default PageTemplate;
